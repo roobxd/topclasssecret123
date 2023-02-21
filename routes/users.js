e15759085371f79f5e4016f072d60827d588602f
Rocco van Baardwijk <rocco.van.baardwijk@hva.nl>
2023-02-21 21:54:26 +0100

    Random Testing
    Pug testing en route testing.

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+// Define a new route
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'New User' });
 });
 
 module.exports = router;
