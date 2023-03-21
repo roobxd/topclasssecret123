d485c8e5b5c41d4507367cd40c024b35b2a0df96
rocco-vb <rocco.van.baardwijk@hva.nl>
2023-03-21 15:04:52 +0100

    Bulletin Posts can be created and misc fixes.
    Bulletin Posts can be created on the right endpoint now, furthermore some minor fixes to signup and other things.

--- src/assets/js/repositories/loadAllUsersRepository.js
+++ src/assets/js/repositories/loadAllUsersRepository.js
@@ -1,23 +1,19 @@
 
 
-import {NetworkManager} from "../framework/utils/networkManager.js";
+import { NetworkManager } from "../framework/utils/networkManager.js";
 
 export class loadAllUsersRepository {
     #networkManager;
     #route;
 
     constructor() {
-
         this.#route = "/loadUsers";
         this.#networkManager = new NetworkManager();
     }
 
 
-    loadUsers( email) {
-
-        return this.#networkManager.doRequest(`${this.#route}/${email}`,
-            "GET")
-
+    loadUsers(email) {
+        return this.#networkManager.doRequest(`${this.#route}/${email}`, "GET")
     }
 
 
