a6ae90b79fe4ec5349e53a0d6f8305c0dec3b72d
K Kifleyesus Musgun Sium <kifleyesus.kifleyesus.musgun.sium@hva.nl>
2023-05-17 19:07:06 +0200

    Tijdlijn css
    The css of tijdlijn is consistent with the website

--- src/assets/js/controllers/tijdlijnController.js
+++ src/assets/js/controllers/tijdlijnController.js
@@ -22,9 +22,7 @@ export class TijdlijnController extends Controller {
         this.#getStories();
 
         console.log(this.#tijdlijnView);
-        this.#tijdlijnView.onclick = function () {
-            this.#getStories();
-        }
+
 
 
     }
@@ -41,9 +39,43 @@ export class TijdlijnController extends Controller {
         // this.#tijdlijnView.querySelector(".timeline").style.animation = "moveline" +  data.result.length + "s linear forwards;"
 
         for (let i = 0; i < data.result.length; i++) {
+            /*
+            Some important calculations for likes and trend would be first calculated
+             */
+
+            // difference of likes and dislikes
+            const likes = data.result[i].aantalLikes
+            const dislikes = data.result[i].aantalDislikes
+            const difference = likes - dislikes;
+            const tienProcent = 10 / 100 * (likes + dislikes);
+            console.log("10%: " + tienProcent);
+            const vijtienProcent = 15 / 100 * (likes + dislikes);
+            console.log("15%: " + vijtienProcent)
+            const twentigProcent = 20 / 100 * (likes + dislikes);
+            console.log("20%: " + twentigProcent)
+            const vijftigProcent = 50 / 100 * (likes + dislikes);
+            console.log("50%: " + vijftigProcent)
+            const honderdProcent = 100 / 100 * (likes + dislikes);
+            console.log("100%: " + honderdProcent);
+
+
+            // the difference between the day of post and of today, in order to determine trending post
+
+            const postDay = data.result[i].jaartalGebeurtenis;
+            const today = new Date();
+            const postDateObj = new Date(postDay);
+            const millisecondsInDay = 1000 * 60 * 60 * 24;
+
+
+            const difference_in_time = today.getTime() - postDateObj.getTime();
+            const difference_in_day = Math.floor(difference_in_time / millisecondsInDay);
+
+
+
+            console.log("Days: " + difference_in_day)
             // Content container includes profiel img, date and story. Container is appended in .timeline
             const container = document.createElement("div");
-            if (i % 2 === 0){
+            if (i % 2 === 0) {
                 container.className = "container right-container";
             } else {
                 container.className = "container left-container";
@@ -51,48 +83,185 @@ export class TijdlijnController extends Controller {
 
             this.#tijdlijnView.querySelector(".timeline").appendChild(container);
 
+            // a div for specific type of stroy based on the likes and/or type of the customer
+            const story = document.createElement("div");
+            story.className = "story";
+            story.classList.add("one");
+            story.classList.add("persoonstory");
+            // story is child of container
+            container.appendChild(story);
+
+            // div for the image
+            const divImage = document.createElement("div");
+            divImage.className = "image";
+            story.appendChild(divImage);
+
             // Image for profile inside container
             const image = document.createElement("img");
             image.alt = "Avatar";
-            image.className = "avatar";
-            image.src = "../assets/img/guus.jpg";
-            container.appendChild(image);
+            image.className = "trendingimage";
+            image.src = "../assets/img/petje.jpg";
+            divImage.appendChild(image);
 
             // Text-box for stroy
-            const textBox = document.createElement("div");
-            textBox.className = "text-box";
-            container.appendChild(textBox);
+            const text = document.createElement("div");
+            text.className = "text";
+            story.appendChild(text);
+
+            // div for text-info
+            const textInfo = document.createElement("div");
+            textInfo.className = "text-info";
+            text.appendChild(textInfo);
+
+            // h2 for the date
+            const date = document.createElement("h2");
+            date.className = "gebeurtenis";
+            date.innerHTML = postDay.slice(0, 10);
+            textInfo.appendChild(date);
+
 
             // title for the story
-            const title = document.createElement("h2");
-            title.className = "title";
+            const title = document.createElement("p");
+            title.className = "story-title";
             title.innerHTML = data.result[i].onderwerp
-            textBox.appendChild(title);
+            textInfo.appendChild(title);
 
-            // date
-            const date = document.createElement("small");
-            date.className = "gebeurtenis";
-            date.innerHTML = data.result[i].jaartalGebeurtenis;
-            textBox.appendChild(date);
 
-            // story
-            const story = document.createElement("p");
-            story.className = "story";
-            story.innerHTML = data.result[i].bericht;
-                textBox.appendChild(story);
+            // story - text
+            const storyText = document.createElement("p");
+            storyText.className = "story-text";
+            storyText.innerHTML = data.result[i].bericht;
+            textInfo.appendChild(storyText);
+
+            // div for the Icons, is child of story div
+            const icons = document.createElement("div");
+            icons.className = "icons";
+            story.appendChild(icons);
+
+            // div for the type of the icon
+            const iconType = document.createElement("div");
+            iconType.className = "trending";
+            icons.appendChild(iconType);
+
+            // the type of the icon would be determined depending on the number of likes of the story
+            if (difference_in_day <= 7) {
+                console.log("Het is niet een week geweest")
+                if (difference >= 0 && difference <= tienProcent ) {
+                    console.log("difference: "+ difference + ", 10%: "+ tienProcent  )
+                    // i for the icons
+                    const heartIcon = document.createElement("i");
+                    heartIcon.className = "bi bi-heart-fill heart";
+                    iconType.appendChild(heartIcon);
+
+                } else if (difference > tienProcent && difference <= twentigProcent ) {
+                    console.log("difference: "+ difference + ", 20%: "+ twentigProcent  )
+
+                    const graphIcon = document.createElement("i");
+                    graphIcon.className = "bi bi-graph-up-arrow";
+                    iconType.appendChild(graphIcon);
+
+                } else if ( difference >= vijftigProcent ) {
+                    console.log("difference: "+ difference + ", 50%: "+ vijftigProcent  )
+
+                    const fireIcon = document.createElement("i");
+                    fireIcon.className = "bi bi-fire trending";
+                    iconType.appendChild(fireIcon);
+
+                } else if (difference < 0 ) {
+                    console.log("difference: "+ difference + ", -10%: min "+ tienProcent)
+                    const canIcon = document.createElement("i");
+                    canIcon.className = "bi bi-trash3-fill trash";
+                    iconType.appendChild(canIcon);
+                } else {
+                    // const heartIcon = document.createElement("i");
+                    // heartIcon.className = "bi bi-heart-fill heart";
+                    // iconType.appendChild(heartIcon);
+                }
+            } else {
+                console.log(" meer dan een week ")
+
+                if (difference <= tienProcent) {
+                    // i for the icons
+                    const heartIcon = document.createElement("i");
+                    heartIcon.className = "bi bi-heart-fill heart";
+                    iconType.appendChild(heartIcon);
+
+                } else if (difference <= twentigProcent ) {
+
+                    const graphIcon = document.createElement("i");
+                    graphIcon.className = "bi bi-graph-up-arrow";
+                    graphIcon.appendChild(heartIcon);
+
+                } else if ( difference <= vijftigProcent ) {
+                    const fireIcon = document.createElement("i");
+                    fireIcon.className = "bi bi-fire trending";
+                    iconType.appendChild(fireIcon);
+
+                } else if (difference < 0 ) {
+                    const canIcon = document.createElement("i");
+                    canIcon.className = "bi bi-trash3-fill trash";
+                    iconType.appendChild(canIcon);
+                } else {
+                    const heartIcon = document.createElement("i");
+                    heartIcon.className = "bi bi-heart-fill heart";
+                    iconType.appendChild(heartIcon);
+                }
+
+            }
+
+
+            // div for readme icon
+            const readme = document.createElement("div")
+            readme.className = " readme";
+            icons.appendChild(readme);
+
+            // icon of readme
+            const readmeIcon = document.createElement("i");
+            readmeIcon.className = "bi bi-arrow-right gonext";
+            readme.appendChild(readmeIcon);
+
+
+            // div for the number of likes/dislikes
+            const differentialLikes = document.createElement("div");
+            differentialLikes.className = "iconsadd";
+            story.appendChild(differentialLikes);
+
+            // holder div for differentialLikes
+            const iconnumber = document.createElement("div");
+            iconnumber.className = "iconnumber";
+            differentialLikes.appendChild(iconnumber);
+
+            // p for the real differentialLikes
+            const likeDifference = document.createElement("p");
+            if (difference > 0) {
+                likeDifference.innerHTML = "+" + difference;
+            } else {
+                likeDifference.innerHTML = difference;
+            }
+
+            iconnumber.appendChild(likeDifference);
+
 
             // arrow
-            const arrow = document.createElement("span");
-            if(i % 2 === 0 ){
+            const arrow = document.createElement("div");
+            if (i % 2 === 0) {
                 arrow.className = "right-container-arrow";
             } else {
                 arrow.className = "left-container-arrow";
             }
-            textBox.appendChild(arrow);
+            differentialLikes.appendChild(arrow);
         }
 
 
-
-
     }
+
+    #trending(length) {
+        //    Change the icons whenever the likes or trend is changing.
+        let iconDiv = document.querySelector()
+        for (let i = 0; i < lenght; i++) {
+
+
+        }
+    }
+
 }
