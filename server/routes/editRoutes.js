c0efb7217b17c6ac98bae7d047ab2c90b9c9dd08
Rocco van Baardwijk <rocco.van.baardwijk@hva.nl>
2023-06-01 17:21:58 +0200

    Added JSDoc for the files Rocco touched or created.
    JS for the above.

--- server/routes/editRoutes.js
+++ server/routes/editRoutes.js
@@ -1,75 +1,71 @@
 /**
- * This class contains ExpressJS routes specific for the Edit entity
- * this file is automatically loaded in app.js
- *
+ * Routes file for edit entity
  * @author Rocco van Baardwijk
  */
 
 
 class EditRoutes {
-    #errorCodes = require("../framework/utils/httpErrorCodes")
-    #databaseHelper = require("../framework/utils/databaseHelper")
-    #app
-    // #id = require("../../src/assets/js/app.js");
+    // Private instance variables for error codes, database helper and express app.
+    #errorCodes = require("../framework/utils/httpErrorCodes");
+    #databaseHelper = require("../framework/utils/databaseHelper");
+    #app;
 
     /**
-     * @param app - ExpressJS instance(web application) we get passed automatically via app.js
+     * Creates a new EditRoutes instance.
+     * @param {Object} app - ExpressJS application instance. 
      * Important: always make sure there is an app parameter in your constructor!
      */
     constructor(app) {
         this.#app = app;
 
-        //call method per route for the rooms entity
+        // Initialize all route handling methods.
         this.#update();
-
         this.#getPost();
     }
 
     /**
-     * Update post endpoint
-     * post request, data is sent by client via body - req.body
+     * Route handler for updating a post.
      * @private
      */
     #update() {
-
         this.#app.post("/edit/update", async (req, res) => {
-
             try {
+                // Execute the update query with the provided data.
                 const data = await this.#databaseHelper.handleQuery({
                     query: "UPDATE posts SET gebruiker = ?, onderwerp = ?, soortBericht = ?, bericht = ?, jaartalGebeurtenis = ?, plaatje = ?, publicatieDatum = CURRENT_TIMESTAMP, commentsenabled = ? WHERE id = ?",
-                    values: [ req.body.gebruiker, req.body.titelinput, req.body.storytype, req.body.storyinput, req.body.dateinput, req.body.fileinput, req.body.yesorno, req.body.postid]
+                    values: [req.body.gebruiker, req.body.titelinput, req.body.storytype, req.body.storyinput, req.body.dateinput, req.body.fileinput, req.body.yesorno, req.body.postid]
                 });
-                //just give all data back as json, could also be empty
-                res.status(this.#errorCodes.HTTP_OK_CODE).json(data);
 
+                // Send response with the HTTP OK status and the JSON data.
+                res.status(this.#errorCodes.HTTP_OK_CODE).json(data);
             } catch (e) {
-                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: e })
+                // Send response with the HTTP BAD REQUEST status and the error message.
+                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: e });
             }
         });
     }
 
-
-
     /**
-     * dummy data example endpoint - rooms (welcome screen)
-     * get request, data is sent by client via url - req.params
+     * Route handler for getting a post.
      * @private
      */
     #getPost() {
         this.#app.get("/edit/:lastnumber", async (req, res) => {
             try {
+                // Execute the select query with the post ID.
                 const data = await this.#databaseHelper.handleQuery({
                     query: "SELECT * FROM `posts` WHERE id = ?",
                     values: [req.params.lastnumber]
                 });
 
-                //just give all data back as json, could also be empty
+                // Send response with the HTTP OK status and the JSON data.
                 res.status(this.#errorCodes.HTTP_OK_CODE).json(data);
             } catch (e) {
-                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: e })
+                // Send response with the HTTP BAD REQUEST status and the error message.
+                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: e });
             }
         });
     }
 }
 
-module.exports = EditRoutes
+module.exports = EditRoutes;
--- src/assets/js/controllers/myPostsController.js
+++ src/assets/js/controllers/myPostsController.js
@@ -1,6 +1,9 @@
 /**
- * Controller voor MyPosts Pagina
+ * Class representing the controller for my posts.
+ * @extends Controller
+ * @author Rocco van Baardwijk
  */
+
 import { App } from "../app.js";
 import { Controller } from "./controller.js"
 import { MyPostsRepository } from "../repositories/myPostsRepository.js";
@@ -10,24 +13,29 @@ export class myPostsController extends Controller {
     #myPostsRepository;
     limitperpage;
     
+    /**
+     * Constructs a new instance of myPostsController.
+     */
     constructor() {
         super();
         this.#myPostsRepository = new MyPostsRepository();
-        this.currentPage = 0; // New instance variable
-        this.cachedData = []; // New instance variable
-        this.originalData = []; // Correct placement
-        this.limitperpage = 3; // Initialize it here
+        this.currentPage = 0;
+        this.cachedData = [];
+        this.originalData = [];
+        this.limitperpage = 3;
         this.#setupView();
         this.#getStories();
     }
-
+    /**
+     * Sets up the view by attaching event handlers to UI elements.
+     * @private
+     */
     async #setupView() {
         this.#myPostsView = await super.loadHtmlIntoContent("html_views/myposts.html")
-    
+
         let backarrow = document.querySelector("#firstSvg");
         let nextarrow = document.querySelector("#secondSvg");
-        let searchbar = document.querySelector(".searchbar");  // Get the search bar input field
-    
+
         backarrow.addEventListener("click", (event) => this.#pageSelector("back"));
         nextarrow.addEventListener("click", (event) => this.#pageSelector("next"));
 
@@ -36,29 +44,34 @@ export class myPostsController extends Controller {
 
         let filterlikes = document.querySelector(".likes");
         filterlikes.addEventListener("click", (event) => this.#sorteerLikes());
-        
+
         let searchpost = document.querySelector(".searchbar-icon");
         searchpost.addEventListener("click", (event) => this.#searchPost());
     }
-    
 
+    /**
+     * Fetches the stories of the user.
+     * @private
+     */
     async #getStories() {
         let userid = App.sessionManager.get("id");
         const storyData = await this.#myPostsRepository.getStories(userid);
-        this.cachedData = storyData; // Save the fetched data into cachedData
-        this.originalData = [...storyData]; // Save a copy of the original data
+        this.cachedData = storyData;
+        this.originalData = [...storyData];
 
-        // Clear the story container
         let container = document.querySelector(".story-container");
         container.innerHTML = "";
 
         this.#showStories();
-        this.#updatePageInfo();  // Update the page info
+        this.#updatePageInfo();
     }
 
+    /**
+     * Sorts the cached stories by type.
+     * @private
+     */
     #sorteerType() {
-        if(this.isSorted == 0) {
-            // Sort cachedData by soortBericht
+        if (this.isSorted == 0) {
             this.cachedData.sort((a, b) => {
                 if (a.soortBericht < b.soortBericht) {
                     return -1;
@@ -68,24 +81,25 @@ export class myPostsController extends Controller {
                 }
                 return 0;
             });
-    
+
             this.isSorted = 1;
         } else {
-            // reset back to original data if already sorted
             this.cachedData = [...this.originalData];
             this.isSorted = 0;
         }
-    
-        // Clear the story container
+
         let container = document.querySelector(".story-container");
         container.innerHTML = "";
 
-        this.#showStories(this.cachedData); // Update the page info
+        this.#showStories(this.cachedData);
     }
 
+    /**
+     * Sorts the cached stories by likes.
+     * @private
+     */
     #sorteerLikes() {
-        if(this.isSorted == 0) {
-            // Sort cachedData by soortBericht
+        if (this.isSorted == 0) {
             this.cachedData.sort((a, b) => {
                 if (a.aantalLikes < b.aantalLikes) {
                     return -1;
@@ -95,50 +109,52 @@ export class myPostsController extends Controller {
                 }
                 return 0;
             });
-    
+
             this.isSorted = 1;
         } else {
-            // reset back to original data if already sorted
             this.cachedData = [...this.originalData];
             this.isSorted = 0;
         }
-    
-        // Clear the story container
+
         let container = document.querySelector(".story-container");
         container.innerHTML = "";
-    
-        this.#showStories(this.cachedData); // Update the page info
+
+        this.#showStories(this.cachedData);
     }
 
+    /**
+     * Filters the stories by the input of the search bar.
+     * @private
+     */
     #searchPost() {
         let searchvalue = document.querySelector(".searchbar").value;
 
         if (searchvalue.trim() === '') {
-            // If search value is empty, reset cachedData to the original data
             this.cachedData = [...this.originalData];
         } else {
-            // Otherwise, filter the originalData
             let filteredData = this.originalData.filter((story) => {
                 return story.onderwerp.toLowerCase().includes(searchvalue.toLowerCase());
             });
             this.cachedData = filteredData;
         }
 
-        this.currentPage = 0; // Reset page to the first one
+        this.currentPage = 0;
 
-        // Clear the story container
         let container = document.querySelector(".story-container");
         container.innerHTML = "";
 
         this.#showStories();
-        this.#updatePageInfo();  // Update the page info
+        this.#updatePageInfo();
     }
-    
 
+    /**
+     * Shows the stories within the current page.
+     * @private
+     */
     #showStories() {
         let start = this.currentPage * this.limitperpage;
         let end = start + this.limitperpage;
-    
+
         for (let index = start; index < end && index < this.cachedData.length; index++) {
             let sid = this.cachedData[index].id;
             let stitle = this.cachedData[index].onderwerp;
@@ -148,115 +164,112 @@ export class myPostsController extends Controller {
             this.#appendStory(stitle, sflow, sid, soort, imagepath);
         }
     }
-    
-    
 
-    #pageSelector(whereto){
+
+    /**
+     * Changes the current page.
+     * @param {string} whereto - The direction of page change ("back" or "next").
+     * @private
+     */
+    #pageSelector(whereto) {
         let container = document.querySelector(".story-container");
         container.innerHTML = "";
-    
-        if(whereto == "back" && this.currentPage > 0){
+
+        if (whereto == "back" && this.currentPage > 0) {
             this.currentPage--;
-        } else if(whereto == "next" && (this.currentPage + 1) * this.limitperpage < this.cachedData.length){
+        } else if (whereto == "next" && (this.currentPage + 1) * this.limitperpage < this.cachedData.length) {
             this.currentPage++;
         }
-    
+
         this.#showStories();
-        this.#updatePageInfo();  // Add this line
+        this.#updatePageInfo();
     }
 
+    /**
+     * Updates the page info displayed in the UI.
+     * @private
+     */
     #updatePageInfo() {
-        // Calculate the total number of pages
         let totalPages = Math.ceil(this.cachedData.length / this.limitperpage);
-        // Get the p element inside the .page-selector
         let pageInfo = document.querySelector(".page-selector p");
-        // Update the text
         pageInfo.textContent = `${this.currentPage + 1} / ${totalPages}`;
     }
 
-    
 
+    /**
+     * Appends a story to the UI.
+     * @param {string} stitle - The story title.
+     * @param {number} sflow - The difference between the number of likes and dislikes.
+     * @param {number} sid - The story ID.
+     * @param {string} soort - The type of the story.
+     * @param {string} imagepath - The path of the story image.
+     * @private
+     */
     #appendStory(stitle, sflow, sid, soort, imagepath) {
         let storygradient = "verhaal-gradient";
-        switch(soort) {
+        switch (soort) {
             case "bulletin":
                 storygradient = "bulletin-gradient"
-            break;
+                break;
 
             case "instantie":
                 storygradient = "instantie-gradient"
-            break;
+                break;
         }
 
-        // Create a new div element with the class "story"
         var storyDiv = document.createElement("div");
         storyDiv.classList.add("story");
         storyDiv.classList.add(storygradient);
 
-        // Create an image element
         var img = document.createElement("img");
         img.src = imagepath;
         img.alt = "";
-      
-        // Create an h2 element
+
         var h2 = document.createElement("h2");
         h2.textContent = stitle;
-      
-        // Create the first icon-holder div
+
         var iconHolder1 = document.createElement("div");
         iconHolder1.classList.add("icon-holder");
-      
-        // Create an SVG element
+
         var svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg1.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         svg1.setAttribute("fill", "currentColor");
         svg1.classList.add("bi", "bi-chat-text-fill");
         svg1.setAttribute("viewBox", "0 0 16 16");
-      
-        // Create a path element inside the SVG
+
         var path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
         path1.setAttribute("d", "M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z");
-      
-        // Append the path to the SVG
+
         svg1.appendChild(path1);
-      
-        // Create a p element
+
         var p1 = document.createElement("p");
         p1.textContent = "52";
-      
-        // Append the SVG and p elements to the first icon-holder div
+
         iconHolder1.appendChild(svg1);
         iconHolder1.appendChild(p1);
-      
-        // Create the second icon-holder div
+
         var iconHolder2 = document.createElement("div");
         iconHolder2.classList.add("icon-holder");
-      
-        // Create an SVG element
+
         var svg2 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg2.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         svg2.setAttribute("fill", "currentColor");
         svg2.classList.add("bi", "bi-graph-up-arrow");
         svg2.setAttribute("viewBox", "0 0 16 16");
-      
-        // Create a path element inside the SVG
+
         var path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
         path2.setAttribute("fill-rule", "evenodd");
-        path2.setAttribute("d", "M0 0h1v15h15v1H0V0Zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5Z");
-      
-        // Append the path to the SVG
+        path2.setAttribute("d", "M0 0h1v15h15v1H0V0Z");
+
         svg2.appendChild(path2);
-      
-        // Create a p element
+
         var p2 = document.createElement("p");
         p2.setAttribute("id", "trending");
         p2.textContent = sflow;
-      
-        // Append the SVG and p elements to the second icon-holder div
+
         iconHolder2.appendChild(svg2);
         iconHolder2.appendChild(p2);
-      
-        // Append the image, h2, first icon-holder div, and second icon-holder div to the story div
+
         storyDiv.appendChild(img);
         storyDiv.appendChild(h2);
         storyDiv.appendChild(iconHolder1);
@@ -275,8 +288,7 @@ export class myPostsController extends Controller {
         editButtonText.textContent = "Edit";
 
         editButton.appendChild(editButtonText);
-      
-        // Append the story div to the document body
+
         let targetElement = document.querySelector(".story-container");
         targetElement.appendChild(storyDiv);
         targetElement.appendChild(deleteButton);
@@ -284,7 +296,7 @@ export class myPostsController extends Controller {
 
         storyDiv.addEventListener("click", () => {
             window.location.href = "/#read/" + sid;
-        })
+        });
 
         deleteButton.addEventListener("click", () => {
             alert("Deleting story post with the id: " + sid);
@@ -295,5 +307,5 @@ export class myPostsController extends Controller {
         editButton.addEventListener("click", () => {
             window.location.href = "/#edit/" + sid;
         });
-      }
+    }
 }
