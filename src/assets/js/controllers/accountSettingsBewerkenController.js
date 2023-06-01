bc93f0f1dc92c4842ffd207f9596548b1f22e925
AatjeD <agyemaa1@ad.hva.nl>
2023-06-02 01:18:39 +0200

    Comments added to code

--- src/assets/js/controllers/accountSettingsBewerkenController.js
+++ src/assets/js/controllers/accountSettingsBewerkenController.js
@@ -1,17 +1,16 @@
 /**
  *
- * Controller repsonsible for all events in Account settings view
+ * Controller responsible for all events in Account settings view
  */
-import {AccountSettingsRepository} from "../repositories/accountSettingsRepository.js";
-import {App} from "../app.js";
-import {Controller} from "./controller.js";
-import {UsersRepository} from "../repositories/usersRepository.js";
+import { AccountSettingsRepository } from "../repositories/accountSettingsRepository.js";
+import { App } from "../app.js";
+import { Controller } from "./controller.js";
+import { UsersRepository } from "../repositories/usersRepository.js";
 
 export class AccountSettingsBewerkenController extends Controller {
     #usersRepository;
     #accountSettingsRepository;
     #accountSettingsBewerkenView;
-
 
     constructor() {
         super();
@@ -19,105 +18,105 @@ export class AccountSettingsBewerkenController extends Controller {
         this.#accountSettingsRepository = new AccountSettingsRepository();
 
         this.#setupView();
-
     }
 
+    // Set up the view and attach event listeners
     async #setupView() {
-        this.#accountSettingsBewerkenView = await super.loadHtmlIntoContent("html_views/accountSettingsBewerken.html");
+        this.#accountSettingsBewerkenView = await super.loadHtmlIntoContent(
+            "html_views/accountSettingsBewerken.html"
+        );
 
+        // Attach event listeners for navigation links
+        this.#accountSettingsBewerkenView
+            .querySelector(".algemeen")
+            .addEventListener("click", (event) => {
+                window.location.href = "#accountSettings";
+            });
+        this.#accountSettingsBewerkenView
+            .querySelector(".instantie")
+            .addEventListener("click", (event) => {
+                window.location.href = "#instantie";
+            });
+        this.#accountSettingsBewerkenView
+            .querySelector(".beveiliging")
+            .addEventListener("click", (event) => {
+                window.location.href = "#beveiliging";
+            });
+        this.#accountSettingsBewerkenView
+            .querySelector(".socialMedia")
+            .addEventListener("click", (event) => {
+                window.location.href = "#socialMedia";
+            });
 
-        this.#accountSettingsBewerkenView.querySelector(".algemeen").addEventListener("click", event => {
-            window.location.href = "#accountSettings";
-        });
-        this.#accountSettingsBewerkenView.querySelector(".instantie").addEventListener("click", event => {
-            window.location.href = "#instantie";
-        });
-        this.#accountSettingsBewerkenView.querySelector(".beveiliging").addEventListener("click", event => {
-            window.location.href = "#beveiliging";
-        });
-        this.#accountSettingsBewerkenView.querySelector(".socialMedia").addEventListener("click", event => {
-            window.location.href = "#socialMedia";
-        });
-        // this.#accountSettingsBewerkenView.querySelector(".bulletinGedrag").addEventListener("click", event => {
-        //     window.location.href = "#bulletinGedrag";
-        // });
+        // Attach event listeners for form interactions
+        this.#accountSettingsBewerkenView
+            .querySelector("#editEmail")
+            .addEventListener("click", (event) => this.#handleEmailUpdate(event));
+        this.#accountSettingsBewerkenView
+            .querySelector("#editName")
+            .addEventListener("click", (event) => this.#handleNameUpdate(event));
 
-        //event listener for identity veranderen knop
-        // this.#accountSettingsView.querySelector("#confirmIdentity").addEventListener("click", event => this.#handleIdentityUpdate(event));
-
+        // Log the current user's ID
         console.log(App.sessionManager.get("userId"));
-
-       this.#accountSettingsBewerkenView.querySelector("#editEmail").addEventListener("click", event => this.#handleEmailUpdate(event));
-       this.#accountSettingsBewerkenView.querySelector("#editName").addEventListener("click", event => this.#handleNameUpdate(event));
-
     }
 
-
-    #handleProfilePicturePreview(event) {
+    // Handle the update of the user's name
+    #handleNameUpdate(event) {
         event.preventDefault();
 
-        const profilePicInput = this.#accountSettingsBewerkenView.querySelector("#profilePic");
-        const profilePicPreview = this.#accountSettingsBewerkenView.querySelector("#profilePicPreview");
-
-        if (profilePicInput.files.length === 0) {
-            profilePicPreview.style.display = "none";
-            return;
-        }
-
-        const profilePicFile = profilePicInput.files[0];
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-            profilePicPreview.src = e.target.result;
-            profilePicPreview.style.display = "block";
-        };
-
-        reader.readAsDataURL(profilePicFile);
-    }
-    #handleNameUpdate(event){
-        event.preventDefault();
-
-
-        const newVoornaam = this.#accountSettingsBewerkenView.querySelector("#voornaam").value;
-        const newTussenvoegsel = this.#accountSettingsBewerkenView.querySelector("#tussenvoegsel").value;
-        const newAchternaam = this.#accountSettingsBewerkenView.querySelector("#achternaam").value;
+        const newVoornaam = this.#accountSettingsBewerkenView.querySelector(
+            "#voornaam"
+        ).value;
+        const newTussenvoegsel = this.#accountSettingsBewerkenView.querySelector(
+            "#tussenvoegsel"
+        ).value;
+        const newAchternaam = this.#accountSettingsBewerkenView.querySelector(
+            "#achternaam"
+        ).value;
         const currentId = App.sessionManager.get("id");
-        const currentName = App.sessionManager.get("voornaam","achternaam")
-
+        const currentName = App.sessionManager.get("voornaam", "achternaam");
 
         this.#accountSettingsRepository
             .updateNaam(currentId, newVoornaam, newAchternaam, newTussenvoegsel)
             .then(() => {
-                this.#accountSettingsBewerkenView.querySelector(".naam-update-message").textContent = "Naam succesvol veranderd!";
-                // this.#accountSettingsBewerkenView.querySelector("#currentEmail").textContent = newEmail;
+                this.#accountSettingsBewerkenView.querySelector(
+                    ".naam-update-message"
+                ).textContent = "Naam succesvol veranderd!";
                 App.sessionManager.set("voornaam", newVoornaam);
                 App.sessionManager.set("achternaam", newAchternaam);
                 App.sessionManager.set("tussenvoegsel", newTussenvoegsel);
                 App.sessionManager.set("id", currentId);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error("Error updating name:", error);
-                this.#accountSettingsBewerkenView.querySelector(".naam-update-message").textContent = "Error updating name: " + error.message;
+                this.#accountSettingsBewerkenView.querySelector(
+                    ".naam-update-message"
+                ).textContent = "Error updating name: " + error.message;
             });
-
     }
+
+    // Handle the update of the user's email
     #handleEmailUpdate(event) {
         event.preventDefault();
 
-        const newEmail = this.#accountSettingsBewerkenView.querySelector("#email").value;
+        const newEmail = this.#accountSettingsBewerkenView.querySelector(
+            "#email"
+        ).value;
         const currentEmail = App.sessionManager.get("email");
 
         this.#accountSettingsRepository
             .updateEmail(currentEmail, newEmail)
             .then(() => {
-                this.#accountSettingsBewerkenView.querySelector(".email-update-message").textContent = "Email updated successfully!";
-                // this.#accountSettingsBewerkenView.querySelector("#currentEmail").textContent = newEmail;
+                this.#accountSettingsBewerkenView.querySelector(
+                    ".email-update-message"
+                ).textContent = "Email updated successfully!";
                 App.sessionManager.set("email", newEmail);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error("Error updating email:", error);
-                this.#accountSettingsBewerkenView.querySelector(".email-update-message").textContent = "Error updating email: " + error.message;
+                this.#accountSettingsBewerkenView.querySelector(
+                    ".email-update-message"
+                ).textContent = "Error updating email: " + error.message;
             });
     }
-
 }
--- src/assets/js/controllers/accountSettingsController.js
+++ src/assets/js/controllers/accountSettingsController.js
@@ -1,20 +1,18 @@
 /**
  *
- * Controller repsonsible for all events in Account settings view
+ * Controller responsible for all events in Account settings view
  */
-import {AccountSettingsRepository} from "../repositories/accountSettingsRepository.js";
-import {App} from "../app.js";
-import {Controller} from "./controller.js";
-import {UsersRepository} from "../repositories/usersRepository.js";
-import {VerificatieRepository} from "../repositories/verificatieRepository.js";
-
+import { AccountSettingsRepository } from "../repositories/accountSettingsRepository.js";
+import { App } from "../app.js";
+import { Controller } from "./controller.js";
+import { UsersRepository } from "../repositories/usersRepository.js";
+import { VerificatieRepository } from "../repositories/verificatieRepository.js";
 
 export class AccountSettingsController extends Controller {
     #usersRepository;
     #geverifierdRepository;
     #accountSettingsRepository;
     #accountSettingsView;
-
 
     constructor() {
         super();
@@ -23,91 +21,101 @@ export class AccountSettingsController extends Controller {
         this.#geverifierdRepository = new VerificatieRepository();
 
         this.#setupView();
-
     }
 
+    // Set up the view and attach event listeners
     async #setupView() {
-        this.#accountSettingsView = await super.loadHtmlIntoContent("html_views/accountSettings.html");
+        this.#accountSettingsView = await super.loadHtmlIntoContent(
+            "html_views/accountSettings.html"
+        );
 
+        // Attach event listeners for navigation links
+        this.#accountSettingsView
+            .querySelector(".algemeen")
+            .addEventListener("click", (event) => {
+                window.location.href = "#accountSettings";
+            });
+        this.#accountSettingsView
+            .querySelector(".instantie")
+            .addEventListener("click", (event) => {
+                window.location.href = "#instantie";
+            });
+        this.#accountSettingsView
+            .querySelector(".beveiliging")
+            .addEventListener("click", (event) => {
+                window.location.href = "#beveiliging";
+            });
+        this.#accountSettingsView
+            .querySelector(".socialMedia")
+            .addEventListener("click", (event) => {
+                window.location.href = "#socialMedia";
+            });
 
-        this.#accountSettingsView.querySelector(".algemeen").addEventListener("click", event => {
-            window.location.href = "#accountSettings";
-        });
-        this.#accountSettingsView.querySelector(".instantie").addEventListener("click", event => {
-            window.location.href = "#instantie";
-        });
-        this.#accountSettingsView.querySelector(".beveiliging").addEventListener("click", event => {
-            window.location.href = "#beveiliging";
-        });
-        this.#accountSettingsView.querySelector(".socialMedia").addEventListener("click", event => {
-            window.location.href = "#socialMedia";
-        });
-        // this.#accountSettingsView.querySelector(".bulletinGedrag").addEventListener("click", event => {
-        //     window.location.href = "#bulletinGedrag";
-        // });
-
-        //event listener for identity veranderen knop
-        // this.#accountSettingsView.querySelector("#confirmIdentity").addEventListener("click", event => this.#handleIdentityUpdate(event));
-
+        // Log the user's email
         console.log(App.sessionManager.get("email"));
-        this.#accountSettingsView.querySelector(".bewerken").addEventListener("click", event => {
-            window.location.href = "#accountSettingsBewerken"
-        })
 
+        // Attach event listener for the "bewerken" button
+        this.#accountSettingsView
+            .querySelector(".bewerken")
+            .addEventListener("click", (event) => {
+                window.location.href = "#accountSettingsBewerken";
+            });
+
+        // Log the user's ID
         console.log(App.sessionManager.get("userId"));
+
+        // Get user information from session manager
         const name = App.sessionManager.get("voornaam");
         const achternaam = App.sessionManager.get("achternaam");
         const email = App.sessionManager.get("email");
-        this.#accountSettingsView.querySelector("#name").textContent = name;
-        // this.#accountSettingsView.querySelector("#achternaam").textContent = achternaam;
-        // this.#accountSettingsView.querySelector("#tussenvoegsel").textContent = achternaam;
-        this.#accountSettingsView.querySelector("#email").textContent = email;
-        // this.#accountSettingsView.querySelector("#editEmail").addEventListener("click", event => this.#handleTextToInput(event));
 
+        // Update user information in the HTML
+        this.#accountSettingsView.querySelector("#name").textContent = name;
+        this.#accountSettingsView.querySelector("#email").textContent = email;
+
+        // Set up social media links and image URLs
         const instagram = App.sessionManager.get("instagram");
         const tiktok = App.sessionManager.get("tiktok");
         const facebook = App.sessionManager.get("facebook");
-        console.log("hierzo")
-        console.log(instagram, tiktok, facebook)
 
         this.#accountSettingsView.querySelector(".instagram-link").href = instagram;
         this.#accountSettingsView.querySelector(".tiktok-link").href = tiktok;
         this.#accountSettingsView.querySelector(".facebook-link").href = facebook;
 
-        // the image URLs
-        this.#accountSettingsView.querySelector(".instagram").src = 'uploads/instagram.png';
-        this.#accountSettingsView.querySelector(".tiktok").src = 'uploads/tik-tok.png';
-        this.#accountSettingsView.querySelector(".facebook").src = 'uploads/facebook.png';
+        this.#accountSettingsView.querySelector(".instagram").src = "uploads/instagram.png";
+        this.#accountSettingsView.querySelector(".tiktok").src = "uploads/tik-tok.png";
+        this.#accountSettingsView.querySelector(".facebook").src = "uploads/facebook.png";
 
-
+        // Load user info asynchronously
         await this.#loadUserInfo();
     }
 
+    // Handle the conversion of text to input for email update
     #handleTextToInput(event) {
         event.preventDefault();
 
         // Get the paragraph element
         const emailParagraaf = this.#accountSettingsView.querySelector("#currentEmail");
 
-        // Check if emailPara is null
+        // Check if emailParagraaf is null
         if (!emailParagraaf) {
             console.log('No element found with id "currentEmail".');
             return;
         }
 
         // Log the found element
-        console.log('Found element:', emailParagraaf);
+        console.log("Found element:", emailParagraaf);
 
         // Create a new input element
         const newInput = document.createElement("input");
         newInput.type = "text";
-        newInput.value = emailParagraaf.textContent;  // set the initial input value to the text of the paragraph
+        newInput.value = emailParagraaf.textContent; // set the initial input value to the text of the paragraph
 
         // Replace the paragraph element with the input element
         emailParagraaf.parentNode.replaceChild(newInput, emailParagraaf);
     }
 
-
+    // Handle the preview of the profile picture
     #handleProfilePicturePreview(event) {
         event.preventDefault();
 
@@ -130,64 +138,41 @@ export class AccountSettingsController extends Controller {
         reader.readAsDataURL(profilePicFile);
     }
 
-
+    // Load user information from the repository and update the view
     async #loadUserInfo() {
-        const userMail = App.sessionManager.get("email")
+        const userMail = App.sessionManager.get("email");
 
         try {
             const users = await this.#accountSettingsRepository.getUsers();
-            console.log(users)
+            console.log(users);
 
             // Find the current user in the list of users
-            const userInfo = users.find(user => user.email === userMail);
-            console.log(userInfo)
+            const userInfo = users.find((user) => user.email === userMail);
+            console.log(userInfo);
 
             if (!userInfo) {
                 console.error("User not found:", userMail);
                 return;
             }
 
+            // Check verification status
             const isGeverifieerd = await this.#geverifierdRepository.verifierResult(userMail);
             let klopt;
-            console.log(isGeverifieerd[0].verificatie)
+
+            console.log(isGeverifieerd[0].verificatie);
 
             if (isGeverifieerd[0].verificatie == 1) {
-                klopt = "geverifieerd"
+                klopt = "geverifieerd";
             } else {
-                klopt = "niet geverifieerd"
+                klopt = "niet geverifieerd";
             }
-            console.log(userInfo.voornaam)
 
             // Update user info in the HTML
-            // this.#accountSettingsView.querySelector("#contact").textContent = userInfo.email;
             this.#accountSettingsView.querySelector("#email").textContent = userInfo.email;
             this.#accountSettingsView.querySelector("#name").textContent = `${userInfo.voornaam} ${userInfo.tussenvoegsel} ${userInfo.achternaam}`;
-            // this.#accountSettingsView.querySelector("#name").textContent = `${userInfo.voornaam} ${userInfo.tussenvoegsel} ${userInfo.achternaam}`;
             this.#accountSettingsView.querySelector(".verificatie-status").textContent = klopt;
-
         } catch (error) {
             console.error("Error loading user info:", error);
         }
-
     }
-
-    // #handleEmailUpdate(event) {
-    //     event.preventDefault();
-    //
-    //     const newEmail = this.#accountSettingsView.querySelector("#email").value;
-    //     const currentEmail = App.sessionManager.get("email");
-    //
-    //     this.#accountSettingsRepository
-    //         .updateEmail(currentEmail, newEmail)
-    //         .then(() => {
-    //             this.#accountSettingsView.querySelector(".email-update-message").textContent = "Email updated successfully!";
-    //             App.sessionManager.set("email", newEmail);
-    //             this.#accountSettingsView.querySelector("#currentEmail").textContent = newEmail;
-    //         })
-    //         .catch(error => {
-    //             console.error("Error updating email:", error);
-    //             this.#accountSettingsView.querySelector(".email-update-message").textContent = "Error updating email: " + error.message;
-    //         });
-    // }
-
 }
